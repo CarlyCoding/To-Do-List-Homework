57bc0dcc95acc112050e42c025fe3619c3c7dff1
CarlyCoding <carly_off@outlook.com>
2022-08-01 14:30:04 +0100

    add task functionality added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,35 @@ import './App.css';
 function App() {
 
   const [items, setItems] = useState  ([
+    {name: "Get Diesel for the car", priority: "high"},
+    {name: "Get shopping", priority: "high"},
+    {name: "Get hair done", priority: 'high'},
+  ]);
 
-  ])
+  const [newItem, setNewItem] = useState("");
 
+  const itemNodes = items.map( (item, index) => {
+    return (
+      <li>
+        { item.name }
+      </li>
+    )
+  });
 
+  const handleItemInput = (event) => {
+    setNewItem(event.target.value);
+  };
+
+  const saveNewItem = (event) => {
+    event.preventDefault();
+    const copyItems = [...items]
+    copyItems.push({
+      name: newItem
+    })
+
+    setItems(copyItems);
+    setNewItem("");
+  };
 
 
   return (
@@ -16,10 +41,19 @@ function App() {
     <hr></hr>
 
     <ul>
-
+      {itemNodes}
     </ul>
 
-    </div>
+    <form onSubmit={saveNewItem}>
+      <label htmlFor = "new-items">Add new task:</label>
+      <input
+      id= "new-item"
+      type= "text"
+      value={newItem}
+      onChange= {handleItemInput}/>
+      <input type= "submit" value="Save new task" />
+      </form>
+  </div>
   );
 }
 
